1eaa7d98a9eddafa60beb40c7cd39052ec13f0ce
Tomas Carnecky <tomas.carnecky@gmail.com>
2014-08-06 18:35:40 +0000

    Add explicit types to help GHC 7.8

--- test/Test.hs
+++ test/Test.hs
@@ -31,14 +31,22 @@ import           Data.Time.Clock.POSIX
 
 
 instance Monad m => Serial m Datum
+
 instance Monad m => Serial m UTCTime where
-    series = decDepth $ (posixSecondsToUTCTime . fromIntegral) <$> series
+    series = decDepth $ fromInt <$> series
+      where
+        fromInt :: Int -> UTCTime
+        fromInt = posixSecondsToUTCTime . fromIntegral
+
 instance Monad m => Serial m ZonedTime where
     series = decDepth $ utcToZonedTime utc <$> series
+
 instance Monad m => Serial m Text where
     series = decDepth $ T.pack <$> series
+
 instance Monad m => Serial m (HashMap Text Datum) where
     series = decDepth $ HMS.fromList <$> series
+
 instance (Monad m, Serial m a) => Serial m (Vector a) where
     series = decDepth $ V.fromList <$> series
 
